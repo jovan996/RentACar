336d4f1bb21bae64d5ca2323a53460017f27dab0
jovan996 <grgurjovan.jg@gmail.com>
2020-04-19 20:44:32 +0200

    dodati identifikatori

--- app/src/main/java/com/example/rentacar/Aktivnosti/DetailViewActivity.java
+++ app/src/main/java/com/example/rentacar/Aktivnosti/DetailViewActivity.java
@@ -95,9 +95,27 @@ public class DetailViewActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         listView = (ListView)findViewById(R.id.detailViewKomentari);
-
-        //TextView naslov = findViewById(R.id.detailViewNaslov);
-        //TextView detalji = findViewById(R.id.detailViewDetalji);
+        slika=(ImageView) findViewById(R.id.detailViewGlavnaSlika);
+        naslov =(TextView) findViewById(R.id.detailViewNaslov);
+        brojSjedista = (TextView) findViewById(R.id.detailViewBrojSedista);
+        brojVrata = (TextView) findViewById(R.id.detailViewBrojVrata);
+        kubikaza = (TextView) findViewById(R.id.detailViewKubikaza);
+        tipMotora= (TextView) findViewById(R.id.detailViewMotor);
+        snagaMotora = (TextView) findViewById(R.id.detailViewSnagaMotora);
+        boja= (TextView) findViewById(R.id.detailViewBoja);
+        godiste= (TextView) findViewById(R.id.detailViewGodiste);
+        kilometraza = (TextView) findViewById(R.id.detailViewKilometraza);
+        cijenaPoDanu = (TextView) findViewById(R.id.detailViewCijeniPoDanu);
+        omiljeni = (CheckBox) findViewById(R.id.detailViewCekirajOmiljeni);
+        ocjena= (RatingBar) findViewById(R.id.detailViewOznaciZvjezdicu);
+        firma = (TextView) findViewById(R.id.detailViewImeFirme);
+        opis = (TextView) findViewById(R.id.detailViewOpisFirme);
+        adresa = (TextView) findViewById(R.id.detailViewFirmaAdresa);
+        grad = (TextView) findViewById(R.id.detailViewFirmaGrad);
+        email= (TextView) findViewById(R.id.detailViewFirmaEmail);
+        brojTelefona = (TextView) findViewById(R.id.brojTelefona);
+        mapa= (MapView) findViewById(R.id.detailMapView);
+        tekstKomentara = (MultiAutoCompleteTextView) findViewById(R.id.detailUnesiKomentar);
 
         imena = new String[]{"Marko", "Slavko", "Zivko"};                //ovi nizovi su samo za potrebe testiranja,svi ovi podaci ce se vuci iz baze
         prezimena = new String[]{"Petovic", "Minic", "Spajic"};
@@ -116,9 +134,7 @@ public class DetailViewActivity extends AppCompatActivity {
         String textNaslov = intent.getStringExtra("naslov");
         String textDetalji = intent.getStringExtra("detalji");
 
-        actionBar.setTitle("Detalji");
-        //naslov.setText(textNaslov);
-        //detalji.setText(textDetalji);
+        actionBar.setTitle("Detalji automobila");
 
         iznajmi = findViewById(R.id.detailViewIznajmiButton);
         iznajmi.setOnClickListener(new View.OnClickListener() {
@@ -133,9 +149,8 @@ public class DetailViewActivity extends AppCompatActivity {
         posaljiKomentar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ovde se izvrsava upit nad bazom
-                /*Intent intent = new Intent(getApplicationContext(), RentActivity.class);
-                startActivityForResult(intent, 0);*/
+                // ovdje se izvrsava upit nad bazom
+
             }
         });
     }
